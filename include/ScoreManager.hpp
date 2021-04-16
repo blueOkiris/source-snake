304d68fd63277e4143b7a097abf9f60f5aca2c6e
Dylan Turner <dylantdmt@gmail.com>
2021-04-16 03:46:59 -0400

    Refactored score manager to utilize polymorphic draw behaviors

--- include/ScoreManager.hpp
+++ include/ScoreManager.hpp
@@ -2,12 +2,48 @@
 
 #include <string>
 #include <fstream>
+#include <memory>
+#include <map>
 #include <SFML/Graphics.hpp>
 #include <GameLoop.hpp>
 #include <ResourceManager.hpp>
 #include <Game.hpp>
 
 namespace snake {
+    class ScoreManager;
+    
+    class ScoreManagerDrawBehavior {
+        public:
+            virtual void draw(
+                const ScoreManager &parent,
+                sf::RenderTarget &target, sf::RenderStates states
+            ) const = 0;
+    };
+    
+    class ScoreManagerStart : public ScoreManagerDrawBehavior {
+        public:
+            void draw(
+                const ScoreManager &parent,
+                sf::RenderTarget &target, sf::RenderStates states
+            ) const override;
+    };
+    
+    class ScoreManagerPlaying : public ScoreManagerDrawBehavior {
+        public:
+            void draw(
+                const ScoreManager &parent,
+                sf::RenderTarget &target, sf::RenderStates states
+            ) const override;
+    };
+    
+    class ScoreManagerDead: public ScoreManagerDrawBehavior {
+        public:
+            void draw(
+                const ScoreManager &parent,
+                sf::RenderTarget &target, sf::RenderStates states
+            ) const override;
+    };
+    
     class ScoreManager : public link::Updatable, public sf::Drawable {
         private:
             GameState _state;
@@ -17,15 +53,21 @@ namespace snake {
             int _score, _highScore;
             const std::string _highScorePath = ".highscore.txt";
             
+            std::map<GameState, std::shared_ptr<ScoreManagerDrawBehavior>>
+                _drawBehaviors;
+            
         public:
             ScoreManager(const link::ResourceManager &resMan);
             
-            void setHighScore();
+            void setHighScore(); // Possibly set highscore to score and save
             void addToScore(int value);
             void resetScore(void);
             
-            int score(void);
-            int highScore(void);
+            // State access
+            const sf::Font &font(void) const;
+            sf::Vector2u winSize(void) const;
+            int score(void) const;
+            int highScore(void) const;
             
             void update(const double delta, link::GameLoop &loop) override;
             void draw(
--- src/ScoreManager.cpp
+++ src/ScoreManager.cpp
@@ -1,5 +1,7 @@
 #include <string>
 #include <fstream>
+#include <memory>
+#include <map>
 #include <SFML/Graphics.hpp>
 #include <GameLoop.hpp>
 #include <ResourceManager.hpp>
@@ -7,9 +9,93 @@
 
 using namespace snake;
 
+void ScoreManagerStart::draw(
+        const ScoreManager &parent,
+        sf::RenderTarget &target, sf::RenderStates states) const {
+    sf::Text highScoreText;
+    highScoreText.setFont(parent.font());
+    highScoreText.setString(
+        "High Score: " + std::to_string(parent.highScore())
+    );
+    highScoreText.setCharacterSize(30);
+    highScoreText.setFillColor(sf::Color(0xFFFFFFFF));
+    auto highScoreCenter = sf::Vector2f(
+        highScoreText.getGlobalBounds().width / 2,
+        highScoreText.getGlobalBounds().height / 2
+    );
+    highScoreText.setOrigin(highScoreCenter);
+    highScoreText.setPosition(sf::Vector2f(
+        parent.winSize().x / 2,
+        parent.winSize().y / 2 + highScoreText.getGlobalBounds().height
+    ));
+    target.draw(highScoreText);
+}
+
+void ScoreManagerPlaying::draw(
+        const ScoreManager &parent,
+        sf::RenderTarget &target, sf::RenderStates states) const {
+    sf::Text scoreText;
+    scoreText.setFont(parent.font());
+    scoreText.setString("Score: " + std::to_string(parent.score()));
+    scoreText.setCharacterSize(16);
+    scoreText.setFillColor(sf::Color(0xFFFFFFFF));
+    scoreText.setPosition(sf::Vector2f(48, 48));
+    target.draw(scoreText);
+}
+
+void ScoreManagerDead::draw(
+        const ScoreManager &parent,
+        sf::RenderTarget &target, sf::RenderStates states) const {
+    sf::Text highScoreText;
+    highScoreText.setFont(parent.font());
+    highScoreText.setString(
+        "High Score: " + std::to_string(parent.highScore())
+    );
+    highScoreText.setCharacterSize(30);
+    highScoreText.setFillColor(sf::Color(0xFFFFFFFF));
+    auto highScoreCenter = sf::Vector2f(
+        highScoreText.getGlobalBounds().width / 2,
+        highScoreText.getGlobalBounds().height / 2
+    );
+    highScoreText.setOrigin(highScoreCenter);
+    highScoreText.setPosition(sf::Vector2f(
+        parent.winSize().x / 2,
+        parent.winSize().y / 2 + 2 * highScoreText.getGlobalBounds().height
+    ));
+    target.draw(highScoreText);
+    
+    sf::Text scoreText;
+    scoreText.setFont(parent.font());
+    scoreText.setString("Score: " + std::to_string(parent.score()));
+    scoreText.setCharacterSize(30);
+    auto scoreTextCenter = sf::Vector2f(
+        scoreText.getGlobalBounds().width / 2,
+        scoreText.getGlobalBounds().height / 2
+    );
+    scoreText.setOrigin(scoreTextCenter);
+    scoreText.setPosition(sf::Vector2f(
+        parent.winSize().x / 2,
+        parent.winSize().y / 2
+            + 2 * highScoreText.getGlobalBounds().height
+            + 2 * scoreText.getGlobalBounds().height
+    ));
+    scoreText.setFillColor(sf::Color(0xFFFFFFFF));
+    target.draw(scoreText);
+}
+
 ScoreManager::ScoreManager(const link::ResourceManager &resMan) :
         _score(0), _highScore(0) {
     _font = resMan.font("font");
+    
+    _drawBehaviors.emplace(
+        GameState::Start, std::make_shared<ScoreManagerStart>()
+    );
+    _drawBehaviors.emplace(
+        GameState::Playing, std::make_shared<ScoreManagerPlaying>()
+    );
+    _drawBehaviors.emplace(
+        GameState::Dead, std::make_shared<ScoreManagerDead>()
+    );
     
     std::ifstream highScoreFile(_highScorePath);
     if(!highScoreFile.is_open()) {
@@ -22,6 +108,14 @@ ScoreManager::ScoreManager(const link::ResourceManager &resMan) :
     }
 }
 
+const sf::Font &ScoreManager::font(void) const {
+    return _font;
+}
+
+sf::Vector2u ScoreManager::winSize(void) const {
+    return _winSize;
+}
+
 void ScoreManager::update(const double delta, link::GameLoop &loop) {
     auto state = std::any_cast<GameState>(loop.globalGameState["state"]);
     _state = state;
@@ -30,75 +124,7 @@ void ScoreManager::update(const double delta, link::GameLoop &loop) {
 
 void ScoreManager::draw(
         sf::RenderTarget &target, sf::RenderStates states) const {
-    switch(_state) {
-        case GameState::Start: {
-            sf::Text highScoreText;
-            highScoreText.setFont(_font);
-            highScoreText.setString(
-                "High Score: " + std::to_string(_highScore)
-            );
-            highScoreText.setCharacterSize(30);
-            highScoreText.setFillColor(sf::Color(0xFFFFFFFF));
-            auto highScoreCenter = sf::Vector2f(
-                highScoreText.getGlobalBounds().width / 2,
-                highScoreText.getGlobalBounds().height / 2
-            );
-            highScoreText.setOrigin(highScoreCenter);
-            highScoreText.setPosition(sf::Vector2f(
-                _winSize.x / 2,
-                _winSize.y / 2 + highScoreText.getGlobalBounds().height
-            ));
-            target.draw(highScoreText);
-        } break;
-        
-        case GameState::Playing: {
-            sf::Text scoreText;
-            scoreText.setFont(_font);
-            scoreText.setString("Score: " + std::to_string(_score));
-            scoreText.setCharacterSize(16);
-            scoreText.setFillColor(sf::Color(0xFFFFFFFF));
-            scoreText.setPosition(sf::Vector2f(48, 48));
-            target.draw(scoreText);
-        } break;
-        
-        case GameState::Dead: {
-            sf::Text highScoreText;
-            highScoreText.setFont(_font);
-            highScoreText.setString(
-                "High Score: " + std::to_string(_highScore)
-            );
-            highScoreText.setCharacterSize(30);
-            highScoreText.setFillColor(sf::Color(0xFFFFFFFF));
-            auto highScoreCenter = sf::Vector2f(
-                highScoreText.getGlobalBounds().width / 2,
-                highScoreText.getGlobalBounds().height / 2
-            );
-            highScoreText.setOrigin(highScoreCenter);
-            highScoreText.setPosition(sf::Vector2f(
-                _winSize.x / 2,
-                _winSize.y / 2 + 2 * highScoreText.getGlobalBounds().height
-            ));
-            target.draw(highScoreText);
-            
-            sf::Text scoreText;
-            scoreText.setFont(_font);
-            scoreText.setString("Score: " + std::to_string(_score));
-            scoreText.setCharacterSize(30);
-            auto scoreTextCenter = sf::Vector2f(
-                scoreText.getGlobalBounds().width / 2,
-                scoreText.getGlobalBounds().height / 2
-            );
-            scoreText.setOrigin(scoreTextCenter);
-            scoreText.setPosition(sf::Vector2f(
-                _winSize.x / 2,
-                _winSize.y / 2
-                    + 2 * highScoreText.getGlobalBounds().height
-                    + 2 * scoreText.getGlobalBounds().height
-            ));
-            scoreText.setFillColor(sf::Color(0xFFFFFFFF));
-            target.draw(scoreText);
-        } break;
-    }
+    _drawBehaviors.at(_state)->draw(*this, target, states);
 }
 
 void ScoreManager::setHighScore() {
@@ -118,10 +144,10 @@ void ScoreManager::resetScore(void) {
     _score = 0;
 }
 
-int ScoreManager::highScore(void) {
+int ScoreManager::highScore(void) const {
     return _highScore;
 }
 
-int ScoreManager::score(void) {
+int ScoreManager::score(void) const {
     return _score;
 }
